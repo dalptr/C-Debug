b923891fd08364a4dce08668358f7069e216f792
Eisen Eisen <87151674+dalptr@users.noreply.github.com>
2023-02-09 17:50:30 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+
+#include "cp_debug.h"
+
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+int main() {
+
+}
